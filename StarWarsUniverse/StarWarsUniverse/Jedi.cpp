dde06980dac969027d8a41d6f2da5b9125de0673
Stoycho Kyosev <68247567+stoychoX@users.noreply.github.com>
2021-05-03 16:46:39 +0300

    Update Jedi.cpp

--- StarWarsUniverse/StarWarsUniverse/Jedi.cpp
+++ StarWarsUniverse/StarWarsUniverse/Jedi.cpp
@@ -1,128 +1,45 @@
-#include "Jedi.h"
+#pragma once
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include "String.h"
 
-void Jedi::copy(const Jedi& other) {
-	name = other.name;
-	rank = other.rank;
-	age = other.age;
-	colorofSaber = other.colorofSaber;
-	power = other.power;
-}
+//nqmam destruktor zashtoto w String si ima
 
+enum class rankofJedi {YOUNGLING, INITIATE, PADAWAN, KNIGHT_ASPIRANT, KNIGHT, MASTER, BATTLE_MASTER,GRAND_MASTER };
 
-Jedi::Jedi() {
-	rank = rankofJedi::YOUNGLING;
-	age = -1;
-	power = 0;
-}
+class Jedi {
+private:
+	String name;
+	rankofJedi rank;
+	int age;
+	String colorofSaber;
+	double power;
+	void copy(const Jedi& other);
+public:
+	Jedi();
+	Jedi(const String _name,const rankofJedi _rank,const int _age,const String _colorofsaber,const double _power);
+	Jedi(const Jedi& other);
+	Jedi& operator=(const Jedi& other);
 
-Jedi::Jedi(const String _name, const rankofJedi _rank, const int _age, const String _colorofsaber, const double _power) {
-	name = _name;
-	rank = _rank;
-	age = _age;
-	colorofSaber = _colorofsaber;
-	power = _power;
-}
-
-Jedi::Jedi(const Jedi& other) {
-	copy(other);
-}
-
-Jedi& Jedi::operator=(const Jedi& other) { //nqmam destroy zashtoto String sam shte si go naprawi
-	if (this != &other) {
-		copy(other);
+	String get_name() const { return name; }
+	rankofJedi get_rankofjedi() const {
+		return rank;
 	}
-	return *this;
-}
-//glavni bukwi
-bool Jedi::operator<(const Jedi& other) {
+	int get_age() const { return age; }
+	String get_colorofsaber() const { return colorofSaber; }
+	double get_power() const { return power; }
 
-	for (size_t i = 0; i < mymin(name.get_size(), other.get_name().get_size()); i++)
-	{
-		if (name[i] < other.get_name()[i])
-		{
-			return true;
-		}
-		else if (name[i] > other.get_name()[i]) return false;
-		//poshov posho
-		//gosho <  goshov true
-	}// gosho < georgi false
-	//petar < stefan true
-	//pavel < pavli
-	if (mymin(name.get_size(), other.get_name().get_size()) == name.get_size())
-	{
-		return true;
-	}
-	return false;
-}
+	void set_jediname(const String& _name) { name = _name; }
+	void set_rankofJedi(const rankofJedi _rank) { rank = _rank; }
+	void set_jediage(int _age) { age = _age; } // da proverq kude tr da imam const i &
+	void set_colorofsaber(const String& _colorofsaber) { colorofSaber = _colorofsaber;}
+	void set_jedipower(const double _power) { power = _power; }
 
-size_t Jedi:: mymin(size_t num1, size_t num2) {
-	if (num1 > num2) return num1;
-	return num2;
-}
+	bool operator<(const Jedi& other);
 
-/*
-void Jedi::promote(double multiplier) {
-	if (multiplier < 0)
-	{
-		std::cout << "The multiplier needs to be a positive number" << std::endl;
-		return;
-	}
-	if(rank == rankofJedi::GRAND_MASTER)
-	{
-		std::cout << "You can't promote jedi from this rank."<<std::endl;
-		return;
-	}
-	switch (rank)
-	{
-	case rankofJedi::YOUNGLING: rank = rankofJedi::INITIATE;
-		break;
-	case rankofJedi::INITIATE: rank = rankofJedi::PADAWAN;
-		break;
-	case rankofJedi::PADAWAN: rank = rankofJedi::KNIGHT_ASPIRANT;
-		break;
-	case rankofJedi::KNIGHT_ASPIRANT: rank = rankofJedi::KNIGHT;
-		break;
-	case rankofJedi::KNIGHT: rank = rankofJedi::MASTER;
-		break;
-	case rankofJedi::MASTER: rank = rankofJedi::BATTLE_MASTER;
-		break;
-	case rankofJedi::BATTLE_MASTER: rank = rankofJedi::GRAND_MASTER;
-		break;
-	default: std::cout << "Invalid rank" << std::endl;
-		break;
-	}
-	power += multiplier * power;
-	return;
-}*/
-/*
-void Jedi::demote(double multiplier) {
+	size_t mymin(size_t num1, size_t num2);
+	//void promote(double multiplier);
+	//void demote(double multiplier);
 
-	if (multiplier < 0) {
-		std::cout << "The multiplier must be a positive number.";
-	}
-	if (rank == rankofJedi::YOUNGLING) {
-		std::cout << "You can't demote a jedi from this rank.";
-		return;
-	}
-	switch (rank)
-	{
-	case rankofJedi::INITIATE: rank = rankofJedi::YOUNGLING;
-		break;
-	case rankofJedi::PADAWAN: rank = rankofJedi::INITIATE;
-		break;
-	case rankofJedi::KNIGHT_ASPIRANT: rank = rankofJedi::PADAWAN;
-		break;
-	case rankofJedi::KNIGHT: rank = rankofJedi::KNIGHT_ASPIRANT;
-		break;
-	case rankofJedi::MASTER: rank = rankofJedi::KNIGHT;
-		break;
-	case rankofJedi::BATTLE_MASTER: rank = rankofJedi::MASTER;
-		break;
-	case rankofJedi::GRAND_MASTER: rank = rankofJedi::BATTLE_MASTER;
-		break;
-	default: std::cout << "Invalid rank.";// ne bi trqbwalo da wliza no wse pak
-		break;
-	}
-	power -= multiplier * power;
-	return;
-}*/
+
+};
