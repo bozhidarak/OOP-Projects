d78764addcc31124b9927618d7e9eba74acf723b
bozhidarak <72149764+bozhidarak@users.noreply.github.com>
2023-03-22 13:06:38 +0200

    Delete 11.06.prakt/11.06.prakt directory

--- 11.06.prakt/11.06.prakt/TimestampWithDescription.cpp
+++
@@ -1,8 +0,0 @@
-#include"TimestampWithDescription.h"
-
-void TimestampWithDescription::SetDescription(std::string _desc) {
-	desc = _desc;
-}
-std::string TimestampWithDescription::GetDescription() const {
-	return desc;
-}
